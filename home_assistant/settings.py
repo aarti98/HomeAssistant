29afdfdd5d4b9e501d23544d23dd328324d6f615
Ubuntu <azureuser@home-assistant-app.vwlxkfnmy2iexksewzmuji3syd.rx.internal.cloudapp.net>
2023-11-27 05:05:48 +0000

    Saving, adding devices

--- home_assistant/settings.py
+++ home_assistant/settings.py
@@ -39,8 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    'rest_framework',
     'users',
-    'rest_framework'
+    'devices',
 ]
 
 MIDDLEWARE = [
